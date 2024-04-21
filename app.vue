b3300658efdb738c80335ee99936b443d1adda9f
Septatrix <24257556+Septatrix@users.noreply.github.com>
2024-04-21 23:29:17 +0200

    Replace default Nuxt favicon

--- app.vue
+++ app.vue
@@ -11,4 +11,8 @@ useSeoMeta({
   title: "CAMPUSoffline",
   description: "The handy search engine for RWTHonline.",
 });
+
+useHead({
+  link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+});
 </script>
